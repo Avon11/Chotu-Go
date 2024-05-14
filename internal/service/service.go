97fefae88cc7b217f9c20a9fe80697de6fdcb871
AbhinavRana <vishuchoudhary131@gmail.com>
2024-05-14 22:33:41 +0530

    Random number generation logic & dominIo Setup

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,3 +1,22 @@
 package service
 
-// Business logic
+import (
+	"crypto/rand"
+	"fmt"
+
+	domainio "github.com/Avon11/Chotu-Go/internal/DomainIo"
+)
+
+func CreateShortUrl(oldUrl string) (shortCode *domainio.ShortCodeDomain, errResp *domainio.ErrorResponse) {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	randomBytes := make([]byte, 6)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for i := 0; i < 6; i++ {
+		randomBytes[i] = charset[int(randomBytes[i])%len(charset)]
+	}
+	fmt.Println("Random string : ", string(randomBytes))
+	return
+}
