00a7440c615bffa8f16125c4f8e8499d40861eff
AbhinavRana <vishuchoudhary131@gmail.com>
2024-05-15 22:49:12 +0530

    Added resp code and error msg. Created package to handle DB operation

--- internal/service/service.go
+++ internal/service/service.go
@@ -2,21 +2,65 @@ package service
 
 import (
 	"crypto/rand"
-	"fmt"
+	"log"
 
+	db "github.com/Avon11/Chotu-Go/internal/DB"
 	domainio "github.com/Avon11/Chotu-Go/internal/DomainIo"
 )
 
+const prefix = "https://chotu.com/" // Replace with frontend url ---- TODO
+
 func CreateShortUrl(oldUrl string) (shortCode *domainio.ShortCodeDomain, errResp *domainio.ErrorResponse) {
+	shortUrlCode := ""
+	for {
+		shortUrlCode = CreateShortCode()
+		notFound := ValidateDupliateKey(shortUrlCode)
+		if !notFound {
+			break
+		}
+	}
+	err := AddShortCodeService(oldUrl, shortUrlCode)
+	if err != nil {
+		errResp = &domainio.ErrorResponse{
+			Error:   err,
+			ErrMsg:  "internal server error",
+			ErrCode: 500,
+		}
+		log.Fatalln("Error while creating short code", err)
+	}
+	shortUrl := prefix + shortUrlCode
+	shortCode = &domainio.ShortCodeDomain{
+		Url: shortUrl,
+	}
+	return
+}
+
+func CreateShortCode() (code string) {
 	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	randomBytes := make([]byte, 6)
 	_, err := rand.Read(randomBytes)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalln("Error while reading random bytes", err)
 	}
 	for i := 0; i < 6; i++ {
 		randomBytes[i] = charset[int(randomBytes[i])%len(charset)]
 	}
-	fmt.Println("Random string : ", string(randomBytes))
+	code = string(randomBytes)
+	return
+}
+
+func AddShortCodeService(url, shortCode string) (err error) {
+	err = db.AddShortCode(shortCode, url)
+	if err != nil {
+		log.Fatalln("Error while storing short code to DB", err)
+	}
+	return
+}
+
+func ValidateDupliateKey(shortCode string) (exist bool) {
+	exist, err := db.CheckForDuplicateKey(shortCode)
+	if err != nil {
+		log.Fatalln("Error while validating shortcode", err)
+	}
 	return
 }
